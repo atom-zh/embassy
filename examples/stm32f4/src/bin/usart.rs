2ed7e1ce2238be30f682e9ec62e66ddcb2b8e989
xiaolongatom <xiaolongatom@gmail.com>
2026-01-21 15:06:40 +0800

    two task ok

--- examples/stm32f4/src/bin/usart.rs
+++ examples/stm32f4/src/bin/usart.rs
@@ -1,31 +1,66 @@
 #![no_std]
 #![no_main]
 
-use cortex_m_rt::entry;
 use defmt::*;
-use embassy_stm32::usart::{Config, Uart};
-use embassy_stm32::{bind_interrupts, peripherals, usart};
+use embassy_executor::Spawner;
+use embassy_stm32::usart::{BufferedUart, BufferedInterruptHandler, Config};
+use embassy_stm32::{bind_interrupts, peripherals};
+use embedded_io_async::Write;
+use embassy_sync::blocking_mutex::raw::NoopRawMutex;
+use embassy_sync::mutex::Mutex;
+use embassy_time::Timer;
+use static_cell::StaticCell;
 use {defmt_rtt as _, panic_probe as _};
 
 bind_interrupts!(struct Irqs {
-    USART3 => usart::InterruptHandler<peripherals::USART3>;
+    USART1 => BufferedInterruptHandler<peripherals::USART1>;
 });
 
-#[entry]
-fn main() -> ! {
-    info!("Hello World!");
+type UsartBus = Mutex<NoopRawMutex, BufferedUart<'static>>;
 
+static USART_BUS: StaticCell<UsartBus> = StaticCell::new();
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
 
-    let config = Config::default();
-    let mut usart = Uart::new_blocking(p.USART3, p.PD9, p.PD8, config).unwrap();
+    let mut config = Config::default();
+    config.baudrate = 115200;
 
-    unwrap!(usart.blocking_write(b"Hello Embassy World!\r\n"));
-    info!("wrote Hello, starting echo");
+    static TX_BUF_CELL: StaticCell<[u8; 256]> = StaticCell::new();
+    static RX_BUF_CELL: StaticCell<[u8; 256]> = StaticCell::new();
 
-    let mut buf = [0u8; 1];
+    let tx_buf = TX_BUF_CELL.init([0u8; 256]);
+    let rx_buf = RX_BUF_CELL.init([0u8; 256]);
+
+    let usart = BufferedUart::new(p.USART1, p.PA10, p.PA9, tx_buf, rx_buf, Irqs, config).unwrap();
+
+    let usart_bus = USART_BUS.init(Mutex::new(usart));
+
+    spawner.spawn(usart_task_a(usart_bus).unwrap());
+    spawner.spawn(usart_task_b(usart_bus).unwrap());
+
     loop {
-        unwrap!(usart.blocking_read(&mut buf));
-        unwrap!(usart.blocking_write(&buf));
+        Timer::after_secs(60).await;
+    }
+}
+
+#[embassy_executor::task]
+async fn usart_task_a(usart_bus: &'static UsartBus) {
+    loop {
+        let mut guard = usart_bus.lock().await;
+        unwrap!(guard.write(b"[Task A] Hello from task A!\r\n").await);
+        drop(guard);
+        Timer::after_secs(1).await;
+    }
+}
+
+#[embassy_executor::task]
+async fn usart_task_b(usart_bus: &'static UsartBus) {
+    loop {
+        let mut guard = usart_bus.lock().await;
+        unwrap!(guard.write(b"[Task B] Hello from task B!\r\n").await);
+        drop(guard);
+        Timer::after_secs(1).await;
     }
 }
